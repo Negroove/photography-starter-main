4a2dc07f503bc6d9eb806ebbc7fcba00b7585a31
Negroove <lucasmontoya15@gmail.com>
2023-11-14 20:33:57 -0300

    se termina homePage

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import MobileNav from './MobileNav';
 
 const Header = () => {
   return (
-    <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+    <header className='fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
       <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
         <Link to={'/'} className='max-w-[200]'>
           <img src={Logo} alt=''></img>
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-import { ImFacebook, ImInstagram, ImYoutube } from 'react-icons/im'
+import { ImFacebook, ImInstagram, ImYoutube,ImWhatsapp  } from 'react-icons/im'
 
 const Socials = () => {
   return <div className='hidden xl:flex ml-24'>
-    sociales
+    <ul className='flex gap-4'>
+      <li>
+        <a href='/' target='_blank'><ImWhatsapp/></a>
+      </li>
+      <li>
+        <a href='https://www.instagram.com/' target='_blank'><ImInstagram/></a>
+      </li>
+      <li>
+        <a href='https://www.youtube.com/' target='_blank'><ImYoutube/></a>
+      </li>
+      <li>
+        <a href='https://www.youtube.com/' target='_blank'><ImFacebook/></a>
+      </li>
+    </ul>
   </div>;
 };
 
